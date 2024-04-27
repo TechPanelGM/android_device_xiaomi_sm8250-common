d62aa3e95b0795de23af01ecacb6aec4aa68cdcb
gio1407 <giomook2003@gmail.com>
2024-04-27 09:48:30 +0000

    Change Device Framework Matrix Location

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -222,6 +222,7 @@ BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
 endif
 
 # VINTF
+# Change Vendor Device Framework matrix location here
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
     vendor/aosp/config/device_framework_matrix.xml
